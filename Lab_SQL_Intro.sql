d16c54aa09dc18f70ead822447b1cc84f397c562
hannahlangen <hel@bl.cx>
2022-01-24 18:00:33 +0100

    SQL Labs

---
+++ Lab_SQL_Intro.sql
@@ -0,0 +1,25 @@
+select * from actor
+select * from address
+select * from category
+select * from city
+select * from country
+select * from customer
+select * from film
+select * from film_actor
+select * from film_category
+select * from film_text
+select * from inventory
+select * from language
+select * from payment
+select * from rental
+select * from staff
+select * from store
+select title from film
+select name as language from language
+--languages are English, Italian, Japanese, Mandarin, French and German
+select store_id from store
+-- there are 2 stores
+select * from store
+select staff_id from staff
+select * from staff
+-- there are 2 employees named Mike and Jon
